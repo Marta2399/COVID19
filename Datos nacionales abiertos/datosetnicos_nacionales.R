a16e07daa40945e1bdb439c6d8d0e20611b74752
Pablo <pbenavides@iteso.mx>
2021-02-23 19:43:04 -0600

    added bar chart

--- Datos nacionales abiertos/datosetnicos_nacionales.R
+++ Datos nacionales abiertos/datosetnicos_nacionales.R
@@ -1,12 +1,13 @@
 library(tidyverse)
-data_raw <- read_csv("200430COVID19MEXICO.csv")
 
-ggplot(data = data_raw, mapping = aes(x = HABLA_LENGUA_INDIG, fill = INTUBADO)) +
-  geom_bar()
+data_raw <- read_csv("Datos nacionales abiertos/200430COVID19MEXICO.csv")
 
 data_raw %>% glimpse()
+
 data_raw %>% 
   mutate_at(
-    HABLA_LENGUA_INDIG = as_factor(HABLA_LENGUA_INDIG)
+    .vars = c("HABLA_LENGUA_INDIG", "INTUBADO"),
+    .funs = as_factor
   ) %>% 
-  ggplot(aes(x = HABLA_LENGUA_INDIG, fill = INTUBADO))
+  ggplot(aes(x = HABLA_LENGUA_INDIG, fill = INTUBADO)) +
+  geom_bar()
