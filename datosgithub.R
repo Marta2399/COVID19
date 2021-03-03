8660bd9d1daa7fc745ee20d7eebf67884567dd52
Pablo <pbenavides@iteso.mx>
2021-03-03 17:11:19 -0600

    carga de datos desde github, graficas

--- datosgithub.R
+++ datosgithub.R
@@ -10,41 +10,40 @@ library(lubridate)
 
 
 
-x < -https://github.com/owid/covid-19-data/blob/master/public/data/jhu/full_data.csv
+datos_mundiales <- read_csv("https://raw.github.com/owid/covid-19-data/master/public/data/jhu/full_data.csv")
 
 
 
 
 #Hacer un dataframe del archivo recuperado de la base de datos
-datosmundialess <- read_csv("1614")
-
-# cambiar a formato timestamp a fecha
-worlddata <- datosmundialess %>% 
-  mutate(
-    date = as.POSIXct(timestamp, origin = "1970-01-01")
-  ) 
+# datosmundialess <- read_csv("1614")
 
 #hace un tidy con solo los datos que se requieren
-worlddata_tidy <- select (worlddata, 
-                          "confirmed", 
-                          "country_region", 
-                          "date", 
-                          "deaths", 
-                          "recovered")
+# worlddata_tidy <- select (worlddata, 
+#                           "confirmed", 
+#                           "country_region", 
+#                           "date", 
+#                           "deaths", 
+#                           "recovered")
 
 #vector para la selección de paises con población similar
-poblacionsimiliar <- c( "Mexico",
+poblacion_similiar <- c("Mexico",
                         "Japan", 
                         "Russia", 
                         "Bangladesh", 
                         "Philippines")
 
-#filtrado para la comparación
-comparison <- filter (worlddata_tidy, 
-                      country_region %in% poblacionsimiliar
-)
-#grafica comparativa
-p <- ggplot(data = comparison) +
-  geom_line(mapping = aes(x = date, y = confirmed, color = country_region) )
+
+p <- datos_mundiales %>%
+  filter(location %in% poblacion_similiar) %>% 
+  ggplot(aes(x = date, y = new_cases, color = location)) +
+  geom_line()
 
 plotly::ggplotly(p)
+
+datos_mundiales %>%
+  ggplot(aes(x = date, y = new_cases, group = location)) +
+  geom_line(color = "grey") +
+  geom_line(data = datos_mundiales %>% filter(location %in% poblacion_similiar),
+            aes(color = location), size = 1) +
+  scale_y_log10()
