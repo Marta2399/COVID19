753113b1b913bfae1c6d663750e0c3512b93b783
JOSE CARLOS GALLARDO ACEVES <jose.carlos.gallardo@ibm.com>
2021-03-10 05:38:03 -0600

    Interactive Dashboard Commit

--- Gráficas PIB y Visitantes.R
+++ Gráficas PIB y Visitantes.R
@@ -1,8 +1,9 @@
 library(readxl)
 library(tidyverse)
 library(tsibble)
+library(feasts)
 
-##Gráfica PIB
+##Gr?fica PIB
 PIB <- read_excel("C:/Users/JoseGallardo/Documents/Ajolotec/PAP/BD_INDICADORES_MACROECONOMICOS.xlsx", sheet = "PIB")
 #Filtrar del dataset solo los datos que sean de Jalisco
 PIB <- PIB[PIB$`Nombre entidad`=="Jalisco",]
@@ -12,40 +13,118 @@ colnames(PIB) <- c("Clave","Estado","Year","Clave_Sector","Nombre_Sector","Activ
 ###PIB$Year <- factor(PIB$Year)
 #Definir los datos que vamos a necesitar para filtar
 a <- ggplot(data=PIB,aes(x=Year,y=PIB))
-#Gráfico boxplot con Jitter
+#Gr?fico boxplot con Jitter
 a + geom_jitter(color=I(3)) + geom_boxplot(alpha=I(0.5)) +
   facet_grid(Actividad~.,scales="free")+
-  ggtitle("Gráficas de PIB por año, separadas por tipo de actividad")
-#Grafíco de barras dividiendo por actividad
+  ggtitle("Gr?ficas de PIB por a?o, separadas por tipo de actividad")
+#Graf?co de barras dividiendo por actividad
 a + geom_col(aes(fill=Actividad)) +
-  ggtitle("Gráfica de PIB por año, señalando tipo de actividad")
+  ggtitle("Gr?fica de PIB por a?o, se?alando tipo de actividad")
 
 a + geom_line()
 
 
-PIB_Tsibble <- PIB %>% as_tsibble(index=Year,key=c(Estado,Nombre_Sector))
-PIB_Tsibble %>% filter(Estado=="Jalisco") %>% distinct(Nombre_Sector)
+PIB_Tsibble <- PIB %>% as_tsibble(index=Year,key=c(Estado,Nombre_Sector,Actividad))
+
+PIB_Tsibble1 <- PIB_Tsibble %>% filter(Estado=="Jalisco") %>% distinct(Nombre_Sector)
 
 
 
 
-##Gráfica de Visitantes extranjeros por entrada aérea, por nacionalidad
+PIB_Tsibble1 %>% ?autoplot()
+
+
+y <- tsibble(Year = 2015:2019,
+             Observation = c(123,39,78,52,110),
+             index = Year)
+autoplot(y)
+?autoplot
+?gafa_stock
+mycolors <- c("blue", "#FFC125", "darkgreen", "darkorange")
+
+
+b <-  PIB_Actividad %>% ggplot(aes(x=Year,y=PIB))
+b + geom_line(aes(color=Actividad)) + scale_y_log10()
+
+PIB_Test <- PIB_Tsibble %>% group_by(Estado,Nombre_Sector,Actividad) %>% 
+  summarise(PIB=sum(PIB))
+
+p1 <- PIB_Test %>% group_by(Estado) %>% 
+  summarize(PIB1=sum(PIB)) %>% 
+  ggplot(aes(x=Year,y=PIB1)) +
+  geom_line(aes(color=Estado),size=1.3) +
+  scale_y_log10() +
+  transition_reveal(PIB1)
+p1
+
+
+
+p2 <- PIB_Test %>% filter(Year==2018) %>%
+  group_by(Estado) %>%
+  summarise(PIB1 = sum(PIB)) %>%
+  filter(PIB1>700000) %>%
+  plot_ly(labels = ~Estado,
+          values = ~PIB1,
+          marker = list(colors = mycolors)) %>%
+  add_pie(hole = 0.2) %>%
+  layout(xaxis = list(zeroline = F,
+                      showline = F,
+                      showticklabels = F,
+                      showgrid = F),
+         yaxis = list(zeroline = F,
+                      showline = F,
+                      showticklabels=F,
+                      showgrid=F))
+p2
+
+p3 <- PIB_Test %>% filter(Year==2018) %>%
+  group_by(Actividad) %>%
+  summarise(PIB1 = sum(PIB))  %>%
+  plot_ly(labels = ~Actividad,
+          values = ~PIB1,
+          marker = list(colors = mycolors)) %>%
+  add_pie(hole = 0.2) %>%
+  layout(xaxis = list(zeroline = F,
+                      showline = F,
+                      showticklabels = F,
+                      showgrid = F),
+         yaxis = list(zeroline = F,
+                      showline = F,
+                      showticklabels=F,
+                      showgrid=F))
+p3
+
+
+?salesdt
+
+
+.
+
+
+
+
+
+
+
+
+
+##Gr?fica de Visitantes extranjeros por entrada a?rea, por nacionalidad
 Visitantes<- read_excel("C:/Users/JoseGallardo/Documents/Ajolotec/PAP/BD_INDICADORES_MACROECONOMICOS.xlsx", sheet = "Visitantes")
 #Filtrar del dataset solo los datos que sean de Jalisco
 Visitantes <- Visitantes[(Visitantes$Aeropuerto=='Puerto Vallarta, Jal.')|(Visitantes$Aeropuerto=='Guadalajara, Jal.'),] 
-#Cambiar el tipo de dato de la columna "Año" para poder filtrar 
-Visitantes$Año <- factor(Visitantes$Año)
+#Cambiar el tipo de dato de la columna "A?o" para poder filtrar 
+Visitantes$A?o <- factor(Visitantes$A?o)
 ##Definir los datos que vamos a necesitar para filtar
-#Filtrando y eliminando ciertas regiones para tener un análisis más limpio
-b <- ggplot(data=Visitantes[(Visitantes$Regiones!="América del Norte"&
+#Filtrando y eliminando ciertas regiones para tener un an?lisis m?s limpio
+b <- ggplot(data=Visitantes[(Visitantes$Regiones!="Am?rica del Norte"&
                                Visitantes$Regiones!="Europa"&
-                               Visitantes$Regiones!="Apátrida"&
+                               Visitantes$Regiones!="Ap?trida"&
                                Visitantes$Regiones!="Islas del Caribe"&
-                               Visitantes$Regiones!="África"),],
+                               Visitantes$Regiones!="?frica"),],
             aes(x=Regiones,y=Entradas))
 ##Grafica de Barras dividiendo entre los dos aeropuertos y separando entre sexo
 b + geom_col(aes(fill=Sexo)) + 
-  facet_grid(Año~Aeropuerto,scales="free") +
+  facet_grid(A?o~Aeropuerto,scales="free") +
   ggtitle("Graficas de visitantes extranjeros divididos por area y por aeropuerto")
 
 
@@ -55,63 +134,67 @@ library(tidyr)
 #Leer del excel los datos y pasarlos al dataframe IMMSS
 IMSS <- read_excel("C:/Users/JoseGallardo/Documents/Ajolotec/PAP/BD_INDICADORES_MACROECONOMICOS.xlsx", sheet = "IMSS")
 #Cambiar el nombre de las columnas para facilitar los calculos
-colnames(IMSS) <- c("División","Grupo","Fracción","Fecha","Trabajadores_Asegurados","Trabajadores_Permanentes","Trabajadores Eventuales")
-#Filtrar las fechas para que solo nos apareca el año (eliminar mes y día)
+colnames(IMSS) <- c("Divisi?n","Grupo","Fracci?n","Fecha","Trabajadores_Asegurados","Trabajadores_Permanentes","Trabajadores Eventuales")
+#Filtrar las fechas para que solo nos apareca el a?o (eliminar mes y d?a)
 IMSS$Fecha <- substring(IMSS$Fecha,1,4)
-#Mdificar la tabla para poder graficar los datos más sencillo
+#Mdificar la tabla para poder graficar los datos m?s sencillo
 IMSS_Filtro <- IMSS %>% gather(Trabajador, Asegurados, c("Trabajadores_Asegurados",
                                                          "Trabajadores_Permanentes","Trabajadores Eventuales"))
-#Definir los parámetros de la gráfica
+#Definir los par?metros de la gr?fica
 Graf1 <- ggplot(data=IMSS_Filtro[IMSS_Filtro$Fecha!=2021,],aes(x=Fecha,y=Asegurados))
-#Gráficar
+#Gr?ficar
 Graf1 + geom_col(aes(fill=Trabajador)) + 
-  ggtitle("Gráfica de trabajadores asegurados por IMSS, señalando el tipo de trabajador")
+  ggtitle("Gr?fica de trabajadores asegurados por IMSS, se?alando el tipo de trabajador")
 
 
 
 #Patrones
 Patrones<- read_excel("C:/Users/JoseGallardo/Documents/Ajolotec/PAP/BD_INDICADORES_MACROECONOMICOS.xlsx",
                       sheet = "Patrones")
-#Filtrar la fecha para que solo lea por año
+#Filtrar la fecha para que solo lea por a?o
 Patrones$Fecha <- substring(Patrones$Fecha,1,4)
-#Definir los parámetros de la gráfica
+#Definir los par?metros de la gr?fica
 Graf2 <- ggplot(data=Patrones[Patrones$Fecha!=2021,],aes(x=Fecha,y=Patrones))
-#Gráficar
+#Gr?ficar
 Graf2 + geom_jitter(aes(color=cve2)) +
   coord_cartesian(ylim=c(0,40000)) +
   theme(axis.text.x = element_text(angle = 45)) + 
-  ggtitle("Gráfica de Paotrones asegurados en IMMS por año, separado por tipo de trabajo")
+  ggtitle("Gr?fica de Paotrones asegurados en IMMS por a?o, separado por tipo de trabajo")
 
 
 
-#Inversión Extranjera Directa por tipo de inversión
+#Inversi?n Extranjera Directa por tipo de inversi?n
 #Definir el Dataset
 IED <-  read_excel("C:/Users/JoseGallardo/Documents/Ajolotec/PAP/BD_INDICADORES_MACROECONOMICOS.xlsx",
                    sheet = "IED1")
-#Filtrar la fecha para solo tomar los años
+#Filtrar la fecha para solo tomar los a?os
 IED$Periodo <- substring(IED$Periodo,7,10)
 #Cambiar nombre de columnas
 colnames(IED) <- c("Fecha","Tipo","Cifras Preliminares","Cifras Revisadas","C")
-#Reformular el dataset para facilitar la graficación
+#Reformular el dataset para facilitar la graficaci?n
 IED_Filtro <- IED %>% gather( Tipo_de_cifra,Cifras, c("Cifras Preliminares","Cifras Revisadas"))
 #Hcarer factor la fecha para facilitar modelado
 IED_Filtro$Fecha <- factor(IED_Filtro$Fecha)
-#Definir valores de gráfica
+#Definir valores de gr?fica
 Graf4 <- ggplot(data=IED_Filtro[IED_Filtro$Cifras>0,],aes(x=Fecha,y=Cifras))
 #Graficar
 Graf4 +  geom_col(aes(fill=Tipo_de_cifra)) + 
-  ggtitle("Inversión Extranjera Directa por tipo de inversión")
+  ggtitle("Inversi?n Extranjera Directa por tipo de inversi?n")
 
 
 
 
-#Características de la población ocupada según la Encuesta Nacional de Ocupación y Empleo
+#Caracter?sticas de la poblaci?n ocupada seg?n la Encuesta Nacional de Ocupaci?n y Empleo
 ENOE <- read_excel("C:/Users/JoseGallardo/Documents/Ajolotec/PAP/BD_INDICADORES_MACROECONOMICOS.xlsx",
                    sheet = "ENOE")
-colnames(ENOE)<-c("Entidad","Posicion","Salud","Actividad","Ocupación","Ingreso","Formal","Personas")
+colnames(ENOE)<-c("Entidad","Posicion","Salud","Actividad","Ocupaci?n","Ingreso","Formal","Personas")
 ENOE <- ENOE[ENOE$Entidad=="Jalisco",]
 
 Graf3 <- ggplot(data=ENOE[ENOE$Actividad!="NA",],aes(x=Actividad,y=Personas))
 Graf3 + geom_col(aes(fill=Formal)) +
   theme(axis.text.x=element_text(angle=45)) + 
-  ggtitle("Cantidad de personas en secotres, señalados por tipo de empleo")
+  ggtitle("Cantidad de personas en secotres, se?alados por tipo de empleo")
+
+
+
+
